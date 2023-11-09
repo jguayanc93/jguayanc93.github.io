8ac09f2b21b275863c0cf0b0688ece4a3b77bdbd
juan carlos <jguayancastillo21@gmail.com>
2023-11-08 22:18:28 -0500

    primer atento

---
+++ funcion.js
@@ -0,0 +1,6 @@
+function handleClick(){
+    if(document.getElementById('toogle2').style.display=="none"){
+        return document.getElementById('toogle2').style.display="flex";
+    }
+    else document.getElementById("toogle2").style.display="none";
+}
